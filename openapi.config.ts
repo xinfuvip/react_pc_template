d9dfa2a8133d688eb5f0a1830772b71813c7f5b6
xinfuvip <liuxinfuvip@163.com>
2023-03-29 14:36:03 +0800

    Update openapi.config.ts

--- openapi.config.ts
+++ openapi.config.ts
@@ -3,7 +3,7 @@ const { generateService } = require('@umijs/openapi')
 // https://www.npmjs.com/package/@umijs/openapi
 
 generateService({
-  schemaPath: 'http://wuyuanqing.bcc-gzbh.baidu.com:8800/v3/api-docs?group=api',
+  schemaPath: '',
   serversPath: './src',
   requestLibPath: "import {request} from '@/request'",
   projectName: 'services'
